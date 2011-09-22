a2c5608b521092c86bfe002fe709916868a3937a
Henke Hedelund <henke.hedelund@gmail.com>
2011-09-23 00:27:57 +0200

    implemented some abstract methods

--- Zkilleman/Db/Adapter/Drizzle.php
+++ Zkilleman/Db/Adapter/Drizzle.php
@@ -32,10 +32,38 @@ class Zkilleman_Db_Adapter_Drizzle extends Zend_Db_Adapter_Abstract
 {
     
     /**
+     *
+     * @var drizzle 
+     */
+    protected static $_drizzle = null;
+    
+    /**
+     * Constructor.
+     * 
+     * @param  array|Zend_Config $config 
+     * @throws Zend_Db_Adapter_Exception
+     * @throws Zkilleman_Db_Adapter_Drizzle_Exception
+     */
+    public function __construct($config)
+    {
+        if (self::$_drizzle === null) {
+            if (!extension_loaded('drizzle')) {
+            
+                require_once 'Zkilleman/Db/Adapter/Drizzle/Exception.php';
+                throw new Zkilleman_Db_Adapter_Drizzle_Exception(
+                        'The Drizzle extension is required for this adapter ' .
+                        'but the extension is not loaded');
+            } else {
+                self::$_drizzle = new drizzle();
+            }
+        }
+        parent::__construct($config); 
+    }
+    
+    /**
      * Creates a connection to the database.
      *
      * @return void
-     * @throws Zkilleman_Db_Adapter_Drizzle_Exception
      */
     protected function _connect()
     {
@@ -43,19 +71,53 @@ class Zkilleman_Db_Adapter_Drizzle extends Zend_Db_Adapter_Abstract
             return;
         }
         
-        if (!extension_loaded('drizzle')) {
+        $host = isset($this->_config['host']) ? 
+                (string) $this->_config['host'] : 'localhost';
+        
+        $port = isset($this->_config['port']) ? 
+                (int) $this->_config['port'] : 4427;
+        
+        // suppress warnings and throw exception instead
+        $this->_connection = @self::$_drizzle->addTcp(
+            $host, 
+            $port, 
+            $this->_config['username'], 
+            $this->_config['password'], 
+            $this->_config['dbname'], 
+            DRIZZLE_CON_NONE // no options
+        );
+        
+        if (!$this->isConnected() || self::$_drizzle->errorCode()) {
+            
+            $this->closeConnection();
             
             require_once 'Zkilleman/Db/Adapter/Drizzle/Exception.php';
             throw new Zkilleman_Db_Adapter_Drizzle_Exception(
-                    'The Drizzle extension is required for this adapter but ' .
-                    'the extension is not loaded');
+                    self::$_drizzle->error());
         }
     }
     
+    /**
+     * Returns a list of the tables in the database.
+     *
+     * @return array
+     */
     public function listTables()
     {
-        require_once 'Zkilleman/Db/Adapter/Drizzle/Exception.php';
-        throw new Zkilleman_Db_Adapter_Drizzle_Exception('Not implemented yet');
+        $result = array();
+        
+        $queryResult = @$this->getConnection()->query('SHOW TABLES');
+        if ($queryResult && $queryResult->buffer()) {
+            while ($row = $queryResult->rowNext()) {
+                $result[] = $row[0];
+            }
+            drizzle_result_free($queryResult);
+        } else {
+            require_once 'Zkilleman/Db/Adapter/Drizzle/Exception.php';
+            throw new Zkilleman_Db_Adapter_Drizzle_Exception(
+                    $this->getConnection()->error());
+        }
+        return $result;
     }
     
     public function describeTable($tableName, $schemaName = null)
@@ -64,16 +126,27 @@ class Zkilleman_Db_Adapter_Drizzle extends Zend_Db_Adapter_Abstract
         throw new Zkilleman_Db_Adapter_Drizzle_Exception('Not implemented yet');
     }
     
+    /**
+     * Test if a connection is active
+     *
+     * @return boolean
+     */
     public function isConnected()
     {
-        require_once 'Zkilleman/Db/Adapter/Drizzle/Exception.php';
-        throw new Zkilleman_Db_Adapter_Drizzle_Exception('Not implemented yet');
+        return (bool) ($this->_connection instanceof DrizzleCon);
     }
     
+    /**
+     * Force the connection to close.
+     *
+     * @return void
+     */
     public function closeConnection()
     {
-        require_once 'Zkilleman/Db/Adapter/Drizzle/Exception.php';
-        throw new Zkilleman_Db_Adapter_Drizzle_Exception('Not implemented yet');
+        if ($this->isConnected()) {
+            $this->_connection->close();
+        }
+        $this->_connection = null;
     }
     
     public function prepare($sql)
